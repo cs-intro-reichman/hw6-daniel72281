0bfeb6e16aa1ff746ebc40fd02331cb4619c8f95
daniel daskal <daskal.daniel@gmail.com>
2024-12-22 16:48:52 +0200

    testing

--- Runigram.java
+++ Runigram.java
@@ -118,8 +118,8 @@ public class Runigram {
 		int b = pixel.getBlue();
 
 		int lum = (int) (0.299 * r + 0.587 * g + 0.114 * b);
-		
-		return   new Color(lum, lum, lum);
+		int roundedLum = (int) (lum);
+		return   new Color(roundedLum, roundedLum, roundedLum);
 	}
 	
 	/**
