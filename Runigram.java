7529118e056e35bedd684e5b49ad465844140172
daniel daskal <daskal.daniel@gmail.com>
2024-12-22 17:11:01 +0200

    testing

--- Runigram.java
+++ Runigram.java
@@ -112,14 +112,10 @@ public class Runigram {
 	// lum = 0.299 * r + 0.587 * g + 0.114 * b, and returns a Color object consisting
 	// the three values r = lum, g = lum, b = lum.
 	private static Color luminance(Color pixel) {
-
-		int r = pixel.getRed();
-		int g = pixel.getGreen();
-		int b = pixel.getBlue();
-
-		int lum = (int) (0.299 * r + 0.587 * g + 0.114 * b);
+		
+		double lum = 0.299 * pixel.getRed() + 0.587 * pixel.getGreen() + 0.114 * pixel.getBlue();
 		int roundedLum = (int) (lum);
-		return   new Color(roundedLum, roundedLum, roundedLum);
+		return new Color(roundedLum,roundedLum,roundedLum);
 	}
 	
 	/**
